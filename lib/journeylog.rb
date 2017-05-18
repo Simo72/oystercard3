a4add9f371c0e951d7f3d00ef5e6ff54ded0430d
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-05-18 16:56:13 +0100

    added journeylog Start method

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -2,11 +2,14 @@ class JourneyLog
 
   attr_reader :journey_class
 
-  def initialize(journey_class = Journey.new)
+  def initialize(journey_class)
     @journey_class = journey_class
+    @current_journey = nil
   end
 
   def start(entry_station)
+    @current_journey = Journey.new(entry_station)
+    entry_station
   end
 
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -2,12 +2,15 @@ require 'journeylog'
 
 describe JourneyLog do
 
-  let (:journey) {double :journey}
-  let (:station) {double :station}
-  let (:journey_class) {double :journey_class, new: journey}
+  #let (:journey) {double :journey}
+  let (:entry_station) {double :station}
+  #let (:journey_class) {double :journey_class, new: journey}
+  subject(:current_journey) {described_class.new(entry_station)}
 
   it { is_expected.to respond_to :journey_class }
   it { is_expected.to respond_to(:start).with(1).argument }
 
-
+  it "Start method returns new journey with entry station" do
+  expect(subject.start(entry_station)).to eq entry_station
+end
 end
